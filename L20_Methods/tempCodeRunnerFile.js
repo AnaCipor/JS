48d2fda010063bf889302dc826f91a17364b7095
Ana Cipor <stitchingwithlove@gmail.com>
2026-08-23 21:54:12 +0300

    Add gitignore and remove temp files

--- L20_Methods/tempCodeRunnerFile.js
+++
@@ -1,5 +0,0 @@
-const numbers = [1, 5, 2, 9, 4];
-
-const sum = numbers.reduce((accumulator, currentValue) =>
-     accumulator + currentValue, 0);
-console.log("Сумма всех элементов массива:", sum);
